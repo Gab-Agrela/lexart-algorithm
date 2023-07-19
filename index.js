f28d1cf9bc697d1b57ec157382e6b67bbae66719
Agrela <gabriel.agrela@hotmail.com>
2023-07-19 16:15:47 -0300

    refactor: Refactor functions

--- index.js
+++ index.js
@@ -1,85 +1,22 @@
 const cyclotron = (particle, matrix) => {
   const lines = [];
-  const particles = ["e", "p", "n"];
+  throwError(particle, matrix);
   for (let i = 0; i < matrix; i += 1) {
     const line = [];
-    if (particles.includes(particle)) {
+    if (particle) {
+      functions[particle](i, line, matrix);
     } else {
       withoutParticle(line, matrix);
     }
     lines.push(line);
   }
-  const cyclotron = (particle, matrix) => {};
+  return lines;
+};
 
-  const withoutParticle = (matrix) => {
-    const lines = [];
-    for (let i = 0; i < matrix; i += 1) {
-      const line = [];
-      for (let index = 0; index < matrix; index += 1) {
-        line.push(1);
-      }
-      lines.push(line);
-    }
-    return lines;
-  };
-
-  const electronParticle = (matrix) => {
-    const lines = [];
-    for (let i = 0; i < matrix; i += 1) {
-      const line = [];
-      if (i === 0) {
-        line.push(..."e".repeat(matrix).split(""));
-      } else {
-        for (let index = 0; index < matrix; index += 1) {
-          index < matrix - 1 ? line.push(1) : line.push("e");
-        }
-      }
-      lines.push(line);
-    }
-    return lines;
-  };
-
-  const neutronParticle = (matrix) => {
-    const lines = [];
-    for (let i = 0; i < matrix; i += 1) {
-      const line = [];
-      if (i === 0) {
-        line.push(..."n".repeat(matrix).split(""));
-      } else {
-        line.push(..."1".repeat(matrix).split(""));
-      }
-      lines.push(line);
-    }
-    return lines;
-  };
-
-  const protonParticle = (matrix) => {
-    const lines = [];
-    for (let i = 0; i < matrix; i += 1) {
-      const line = [];
-      for (let index = 0; index < matrix; index += 1) {
-        if (i === 0) {
-          line.push("p");
-        } else if (i < matrix - 2) {
-          index === 0 || index === matrix - 1 ? line.push("p") : line.push(1);
-        } else if (i === matrix - 2) {
-          index === 0 || index >= matrix - 2 ? line.push("p") : line.push(1);
-        } else {
-          index !== matrix - 1 ? line.push("p") : line.push(1);
-        }
-      }
-      lines.push(line);
-    }
-    return lines;
-  };
-
-  // withoutParticle(4).forEach((line) => console.log(line));
-  // console.log("--------------------");
-  // electronParticle(4).forEach((line) => console.log(line));
-  // console.log("--------------------");
-  // neutronParticle(4).forEach((line) => console.log(line));
-  // console.log("--------------------");
-  protonParticle(6).forEach((line) => console.log(line));
+const functions = {
+  n: (i, line, matrix) => neutronParticle(i, line, matrix),
+  e: (i, line, matrix) => electronParticle(i, line, matrix),
+  p: (i, line, matrix) => protonParticle(i, line, matrix),
 };
 
 const withoutParticle = (line, matrix) => {
@@ -88,60 +25,47 @@ const withoutParticle = (line, matrix) => {
   }
 };
 
-const electronParticle = (matrix) => {
-  const lines = [];
-  for (let i = 0; i < matrix; i += 1) {
-    const line = [];
-    if (i === 0) {
-      line.push(..."e".repeat(matrix).split(""));
-    } else {
-      for (let index = 0; index < matrix; index += 1) {
-        index < matrix - 1 ? line.push(1) : line.push("e");
-      }
-    }
-    lines.push(line);
+const neutronParticle = (i, line, matrix) => {
+  if (i === 0) {
+    line.push(..."n".repeat(matrix).split(""));
+  } else {
+    line.push(..."1".repeat(matrix).split(""));
   }
-  return lines;
 };
 
-const neutronParticle = (matrix) => {
-  const lines = [];
-  for (let i = 0; i < matrix; i += 1) {
-    const line = [];
-    if (i === 0) {
-      line.push(..."n".repeat(matrix).split(""));
-    } else {
-      line.push(..."1".repeat(matrix).split(""));
-    }
-    lines.push(line);
-  }
-  return lines;
-};
-
-const protonParticle = (matrix) => {
-  const lines = [];
-  for (let i = 0; i < matrix; i += 1) {
-    const line = [];
+const electronParticle = (i, line, matrix) => {
+  if (i === 0) {
+    line.push(..."e".repeat(matrix).split(""));
+  } else {
     for (let index = 0; index < matrix; index += 1) {
-      if (i === 0) {
-        line.push("p");
-      } else if (i < matrix - 2) {
-        index === 0 || index === matrix - 1 ? line.push("p") : line.push(1);
-      } else if (i === matrix - 2) {
-        index === 0 || index >= matrix - 2 ? line.push("p") : line.push(1);
-      } else {
-        index !== matrix - 1 ? line.push("p") : line.push(1);
-      }
+      index < matrix - 1 ? line.push(1) : line.push("e");
     }
-    lines.push(line);
   }
-  return lines;
 };
 
-// withoutParticle(4).forEach((line) => console.log(line));
-// console.log("--------------------");
-// electronParticle(4).forEach((line) => console.log(line));
-// console.log("--------------------");
-// neutronParticle(4).forEach((line) => console.log(line));
-// console.log("--------------------");
-protonParticle(6).forEach((line) => console.log(line));
+const protonParticle = (i, line, matrix) => {
+  for (let index = 0; index < matrix; index += 1) {
+    if (i === 0) {
+      line.push("p");
+    } else if (i < matrix - 2) {
+      index === 0 || index === matrix - 1 ? line.push("p") : line.push(1);
+    } else if (i === matrix - 2) {
+      index === 0 || index >= matrix - 2 ? line.push("p") : line.push(1);
+    } else {
+      index !== matrix - 1 ? line.push("p") : line.push(1);
+    }
+  }
+};
+
+const throwError = (particle, matrix) => {
+  if (matrix < 4) {
+    throw new Error(
+      "Invalid arguments: matrix must be greater than or equal to 4."
+    );
+  }
+  if (!["e", "p", "n", ""].includes(particle)) {
+    throw new Error('Invalid arguments: particle must be "e", "p", "n" or "".');
+  }
+};
+
+cyclotron("n", 6).forEach((line) => console.log(line));
